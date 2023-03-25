ac0d36f01d4495f7cf7644446fb5b51489357ff3
arpita <arpita2002as@gmail.com>
2023-03-26 01:59:59 +0530

    Added Dashboard

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,6 @@
-import CreateTeam from "./CreateTeam";
 import DashboardNavbar from "./DashboardNavbar";
-import Myteams from "./Myteams";
-import AddTeamMembers from "./AddTeamMembers";
-import { User } from "../../User/User";
+import Sessions from "./sessions";
+import Userpassword from "./Userpassword";
 
 function Dashboard() {
   // const { user } = User();
@@ -10,34 +8,25 @@ function Dashboard() {
   return (
     <>
       <DashboardNavbar />
-      <div className="md:flex mt-[30px] md:mt-[15px] md:flex-row flex flex-col mx-10">
+      <div className="md:flex mt-[30px] md:mt-[15px] md:flex-row flex flex-col mx-4 md:mx-10">
         {/* left dashboard  */}
-        <div className="left m-4 w-[70%]">
+        <div className="left m-4 w-full md:w-[50%]">
           <div className="flex flex-col flex-wrap w-full h-[70px]">
             {/* user infomation box  */}
-            <div className="bg-pink-400 ml-[-12px] my-2 w-full rounded-md">
+            <div className="bg-semilight ml-[-12px] my-2 w-full rounded-md">
               <p className="text-2xl p-4 text-brown">
-                Hello, <span className="text-red-600">sambhav</span>
+                Hello, <span className="text-dark font-bold">sambhav</span>
               </p>
             </div>
+
             {/* user information box ends  */}
           </div>
+          <Userpassword />
         </div>
 
-        {/* center dashboard  */}
-        <div className="center m-4 ml-0 w-full">
-          <div className="flex flex-col w-full ">
-            {/* create a team section  */}
-            {/* <CreateTeam /> */}
-            {/* add team members to team  */}
-            {/* <AddTeamMembers /> */}
-            {/* <div className="bg-light my-2 w-full h-[100px] rounded-md mx-1 box-border"></div> */}
-          </div>
-        </div>
-
-        {/* right dashboard */}
+        {/* sessions */}
         <div className="m-4 ml-0 w-full h-full ">
-          {/* <Myteams /> */}
+          <Sessions />
         </div>
       </div>
     </>
