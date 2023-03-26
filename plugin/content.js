ffbcd4dd4e882ac2c64d8bb8e31e927437155327
samjain233 <94921996+samjain233@users.noreply.github.com>
2023-03-26 08:45:43 +0530

    establishing comm between content.js and event.js

--- plugin/content.js
+++ plugin/content.js
@@ -1,48 +1,37 @@
-//for chekcking signup and login page
-const check = document.querySelectorAll('[type="password"]');
-const login = document.querySelector('[type="password"]');
+(async () => {
+  //for chekcking signup and login page
+  const check = document.querySelectorAll('[type="password"]');
+  const login = document.querySelector('[type="password"]');
 
-if (check.length == 1) {
-  //login
-  if (login) {
-    let url = "blank";
-    chrome.runtime.onMessage.addListener((obj,sender,response)=>{
-        url = obj.url;
-    });
-    const btn = document.createElement("button");
-    btn.innerHTML = "click";
+  if (check.length == 1) {
+    //login
+    if (login) {
+      const btn = document.createElement("button");
+      btn.innerHTML = "click";
+      login.insertAdjacentElement("afterend", btn);
 
-    btn.addEventListener("click", async (e) => {
-      e.preventDefault();
-      const debug = document.createElement("p");
+      btn.addEventListener("click", async (e) => {
+        e.preventDefault();
 
-      const pass = document.createElement("p");
-      pass.innerHTML = login.value;
+        login.value = "sambhav";
 
-      const token = localStorage.getItem("userHack");
-      const web = "https://www.amazon.in/";
+        let port = chrome.runtime.connect({name: "hack36"});
 
-      const response = await fetch("http://localhost:5000/getstring", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ token, web, pass: login.value }),
+
+        const password = await chrome.runtime.sendMessage({
+          type: "givePass",
+          options: {
+            pass: login.value,
+          },
+        });
+
+        login.value = password.password;
+        
+        
       });
-
-      const object = await response.json();
-      if (object.success) {
-        // const result = await password(object.str1, article.value, object.str2);
-        pass.innerHTML = object.password;
-        login.value = object.password;
-      }
-      debug.innerHTML = token;
-
-      document.body.appendChild(pass);
-      document.body.appendChild(debug);
-    });
-
-    login.insertAdjacentElement("afterend", btn);
+    }
+    if (check.length == 2) {
+      //signup
+    }
   }
-  if (check.length == 2) {
-    //signup
-  }
-}
+})();
--- plugin/eventPage.js
+++ plugin/eventPage.js
@@ -1,10 +1,47 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  const data = localStorage.getItem("userHack");
-  console.log(data);
-  if (tab.url) {
-    console.log(tab.url);
-    chrome.tabs.sendMessage(tabId, {
-      url: tab.url,
-    });
+chrome.runtime.onConnect.addListener(function(port) {
+console.assert(port.name === "knockknock");
+
+});
+
+
+
+
+chrome.runtime.onMessage.addListener(async function (
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.type == "givePass") {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      async function (tabs) {
+        const url = tabs[0].url;
+        let flag = false;
+        let end = url.length;
+        for (let i = 0; i < url.length; i++) {
+          if (url[i] == ".") {
+            flag = true;
+          }
+          if (url[i] == "/" && flag) {
+            end = i;
+            break;
+          }
+        }
+        const web = url.slice(0, end);
+        console.log(web);
+        const token = localStorage.getItem("userHack");
+        console.log(token);
+        console.log(request.options);
+        
+        const response = await fetch("http://localhost:5000/getstring", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ token, web, pass: request.options.pass }),
+        });
+        const object = await response.json();
+        console.log(object);
+        sendResponse(object);
+      }
+    );
   }
 });
